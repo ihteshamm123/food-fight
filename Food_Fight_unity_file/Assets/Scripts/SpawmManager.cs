13923527bbdb0269f560a5daacbead1be9c8cbd2
ihteshamm123 <39794328+ihteshamm123@users.noreply.github.com>
2020-05-16 13:11:32 -0700

    update

---
+++ Food_Fight_unity_file/Assets/Scripts/SpawmManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawmManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawmPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+                0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawmPos
+                , animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
