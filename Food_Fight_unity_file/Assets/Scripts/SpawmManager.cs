a8d00db75241bac10cf3d777118defa7474d2b67
ihteshamm123 <39794328+ihteshamm123@users.noreply.github.com>
2020-05-16 14:26:04 -0700

    update

--- Food_Fight_unity_file/Assets/Scripts/SpawmManager.cs
+++ Food_Fight_unity_file/Assets/Scripts/SpawmManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SpawmManager : MonoBehaviour
 {
+
+    private float startDelay = 2;
+    private float spawnInterval = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
+
 
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
@@ -17,6 +21,9 @@ public class SpawmManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+    void SpawnRandomAnimal() {
         if (Input.GetKeyDown(KeyCode.S))
         {
             Vector3 spawmPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
